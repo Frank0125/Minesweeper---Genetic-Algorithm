cc1475e9bc3aebc14e99777046e4e3486175bfd1
Frank0125 <a01252974@tec.mx>
2024-08-14 19:00:32 -0700

    RPS ahi va

--- minesweeper.py
+++ minesweeper.py
@@ -1,52 +1,64 @@
 import random
 
-class plays:
+class choices:
 	rock = 'rock'
 	paper = 'paper'
 	scissors = 'scissors'
-
-
+	
+	
 class agent:
-        score = 0
-        plays = []
+    score = 0
+    plays = ["rock", "rock", "paper", "paper", "scissors", "scissors"]
+    answers = [0,0,0,0,0,0] #which plays are correct
 
 
 def win_condition_RPS (action_agent1, action_agent2): #from the perspective of agent1
-        if action_agent1 == action_agent2:
-                return 0 #draw
-        elif (action_agent1 == plays.paper and action_agent2 == plays.rock) or (action_agent1 == plays.rock and action_agent2 == plays.scissors) or (action_agent1 == plays.scissors and action_agent2 == plays.paper):
-                return 1 #agent1 wins
-        elif (action_agent1 == plays.rock and action_agent2 == plays.paper) or (action_agent1 == plays.scissors and action_agent2 == plays.rock) or (action_agent1 == plays.paper and action_agent2 == plays.scissors):
-                return -1 #agent1 loses
+    if action_agent1 == action_agent2:
+            return 0 #draw
+    elif (action_agent1 == choices.paper and action_agent2 == choices.rock) or (action_agent1 == choices.rock and action_agent2 == choices.scissors) or (action_agent1 == choices.scissors and action_agent2 == choices.paper):
+            return 1 #agent1 wins
+    elif (action_agent1 == choices.rock and action_agent2 == choices.paper) or (action_agent1 == choices.scissors and action_agent2 == choices.rock) or (action_agent1 == choices.paper and action_agent2 == choices.scissors):
+            return -1 #agent1 loses
     
 
 def choose_action ():
-        action = random.choice([plays.rock, plays.paper, plays.scissors])
-        return action
+    action = random.choice([choices.rock, choices.paper, choices.scissors])
+    return action
 
 def print_plays (play1, play2):
-        print("Agent 1 played: ", play1)
-        print("Agent 2 played: ", play2)    
+    print("Agent 1 played: ", play1)
+    print("Agent 2 played: ", play2)    
 
-def play_RPS (agent1, agent2):
-        action_agent1 = choose_action()
-        action_agent2 = choose_action()
-        agent1.plays.append(action_agent1)
-        agent2.plays.append(action_agent2)
-        print_plays(action_agent1, action_agent2)
-        result = win_condition_RPS(action_agent1, action_agent2)
-        agent1.score += result
-        print("Agent 1 score: ", agent1.score)
-        return result
+def play_RPS (agent1, agent2, iteration):
+    action_agent1 = newPlay() 
+    agent1.plays.append(action_agent1)
+    print_plays(action_agent1, agent2.play[iteration])
+    result = win_condition_RPS(action_agent1, agent2.play[iteration])
+    agent1.score += result
+    print("Agent 1 score: ", agent1.score)
+    return result
 
 
 def multiple_plays (agent1, agent2, n):
-        for i in range(n):
-                play_RPS(agent1, agent2)
-        print("Agent 1 plays: ", agent1.plays)
+    for i in range(n):
+            play_RPS(agent1, agent2, i)
+    print("Agent 1 plays: ", agent1.plays)
         
+def newPlay(answer, play):
+    if answer == 1 :
+          return play
+    else :
+        return choose_action()
+
 
 agent1 = agent()
 agent2 = agent()
-
 multiple_plays(agent1, agent2, 6)
+
+""" 
+
+Pasos del juego
+
+1. 
+
+ """
